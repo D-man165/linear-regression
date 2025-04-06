c199f9f7e7b031ffc08634649fe261624aa3e07d
D-man165 <diptiman.bora@gmail.com>
2025-04-06 12:10:07 -0700

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,6 @@
-# Building a Linear Regression model from scratch without use of Scikit-Learn using Gradient Descent and MSE Cost function
+# Building a Lnear Regression model from scratch without use of Scikit-Learn using Gradient Descent and MSE Cost function
 import numpy as np
 import math
-import pandas as pd
 
 class LinearRegression:
     def __init__(self, learning_rate=0.01, n_iters=1000):
@@ -9,10 +8,12 @@ class LinearRegression:
         self.niters = n_iters
         self.weights = 0
         self.bias = 0
+        self.samples = 1
+        self.features = 1
 
     def cost_function(self, y_train, x_train):
         """
-        measures deviation from known dataset(y_train)
+        Measures deviation from known dataset(y_train)
         """
         y_pred = x_train @ self.weights.T + self.bias
         cost = np.mean(np.square(y_pred - y_train))
@@ -20,20 +21,20 @@ class LinearRegression:
     
     def gradient_descent(self, y_train, x_train):
         """
-        returns the changes needed in values of m and c
-        below formulae are easily calclated through partial derivatives of cost function
+        Returns the changes needed in values of m and c
+        Below formulae are easily calclated through partial derivatives of cost function
         """
         y_pred = x_train @ self.weights.T + self.bias
-        dw = 2 * (x_train.T @ (y_pred - y_train))
+        dw = 2 * (1/self.samples) * (x_train.T @ (y_pred - y_train))
         db = 2 * np.mean(y_pred - y_train)
         return dw, db
     
     def fit(self, y_train, x_train):
         """
-        fits the training data and uses above two functions to adjust values of ma and c
+        Fits the training data and uses above two functions to adjust values of ma and c
         """
-        n_samples, n_features = x_train.shape
-        self.weights = np.zeros(n_features)
+        self.samples, self.features = x_train.shape
+        self.weights = np.zeros(self.features)
         self.bias = 0
 
         prev_cost = math.inf # begins with a large value for previous cost value
@@ -42,25 +43,24 @@ class LinearRegression:
             self.weights -= self.lrate * dw
             self.bias -= self.lrate * db
             cost = self.cost_function(y_train, x_train)
-            if abs(prev_cost - cost) < 0.00001: # in case of stagnancy
+            if abs(prev_cost - cost) < 0.0001: # in case of stagnancy
                 print("Model has converged")
                 print(f"Iteration {iter}: m = {np.round(self.weights, 4)}, c = {round(self.bias, 4)}, cost = {round(cost, 4)}")
                 return
-            
             if iter % 100 == 0: # once every 100 epochs
                 print(f"Iteration {iter}: m = {np.round(self.weights, 4)}, c = {round(self.bias, 4)}, cost = {round(cost, 4)}")
             prev_cost = cost
 
     def predict(self, x_test):
         """
-        returns the predicted array
+        Returns the predicted array
         """
         y_pred = x_test @ self.weights.T + self.bias
         return y_pred
     
     def evaluate(self, y_test, x_test):
         """
-        returns the Mean Absolute Percentage Error
+        Returns the Mean Absolute Percentage Error
         very good for relative error
         """
         y_pred = self.predict(x_test)
